f74f893b2900c8e1f08321529c246210701119a1
Anton Paluykh <anton.paluykh@gmail.com>
2021-03-09 19:02:29 +0300

    docs: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function check(str, bracketsConfig) {
+module.exports = function check(input, bracketsConfig) {
   // your solution
+  let stack = [];
+  let existBrackets = {};
+  for(let i = 0; i < bracketsConfig.length; i++) {
+    existBrackets[bracketsConfig[i][0]] = bracketsConfig[i][1];
+  }
+
+  stack.push(input[0]);
+
+  for(let j = 1; j < input.length; j++) {
+    if(input[j] === existBrackets[stack[stack.length - 1]]) {
+      stack.pop();
+    } else {
+      stack.push(input[j]);
+    }
+  }
+  if (stack.length > 0) {
+      return false;
+    }
+  return true;
 }
